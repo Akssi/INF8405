892058ba59772f5febf67ebb4c45b8e782f563d6
amfortin <amfortin@gosecure.ca>
2018-02-11 17:44:43 -0500

    Started work on wifi scanner using LiveData

---
+++ WifiSearcher/app/src/main/java/us/wifisearcher/services/WifiScanner.java
@@ -0,0 +1,56 @@
+package us.wifisearcher.services;
+
+import android.arch.lifecycle.LiveData;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+
+import java.util.List;
+
+public class WifiScanner extends LiveData<List<ScanResult>> {
+    private WifiManager wifiManager;
+    private Context context;
+    private BroadcastReceiver broadcastReceiver;
+    private List<ScanResult> discoveredNetworks;
+
+    public WifiScanner(Context context) {
+        this.context = context;
+        this.wifiManager = (WifiManager) this.context.getSystemService(Context.WIFI_SERVICE);
+        this.wifiManager.setWifiEnabled(true);
+        this.broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                onScanResults(context, intent);
+            }
+        };
+    }
+
+    private void onScanResults(Context context, Intent intent) {
+        discoveredNetworks = wifiManager.getScanResults();
+        if (discoveredNetworks.isEmpty()) {
+            setValue(null);
+        } else {
+            setValue(discoveredNetworks);
+        }
+    }
+
+    private void executeScan() {
+        wifiManager.startScan();
+    }
+
+    @Override
+    protected void onActive() {
+        super.onActive();
+        this.context.registerReceiver(this.broadcastReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        executeScan();
+    }
+
+    @Override
+    protected void onInactive() {
+        super.onInactive();
+        this.context.unregisterReceiver(broadcastReceiver);
+    }
+}
