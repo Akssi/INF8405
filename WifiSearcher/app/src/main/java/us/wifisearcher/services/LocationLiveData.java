50ffebb1a237a380d973848d5c38b64d345a255e
amfortin <amfortin@gosecure.ca>
2018-02-11 21:53:00 -0500

    Wrapped Location services in LiveData

---
+++ WifiSearcher/app/src/main/java/us/wifisearcher/services/LocationLiveData.java
@@ -0,0 +1,56 @@
+package us.wifisearcher.services;
+
+import android.Manifest;
+import android.arch.lifecycle.LiveData;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import com.google.android.gms.location.*;
+
+public class LocationLiveData extends LiveData<Location> {
+    private final Context context;
+    private FusedLocationProviderClient fusedLocationProviderClient = null;
+    private LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location newLocation = locationResult.getLastLocation();
+            setValue(newLocation);
+        }
+    };
+
+    public LocationLiveData(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    private FusedLocationProviderClient getFusedLocationProviderClient() {
+        if (fusedLocationProviderClient == null) {
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
+        }
+        return fusedLocationProviderClient;
+    }
+
+    @Override
+    protected void onActive() {
+        super.onActive();
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        FusedLocationProviderClient locationProviderClient = getFusedLocationProviderClient();
+        LocationRequest locationRequest = LocationRequest.create();
+        Looper looper = Looper.myLooper();
+        locationProviderClient.requestLocationUpdates(locationRequest, locationCallback, looper);
+    }
+
+    @Override
+    protected void onInactive() {
+        if (fusedLocationProviderClient != null) {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        }
+    }
+
+}
