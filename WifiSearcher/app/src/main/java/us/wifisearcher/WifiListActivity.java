5cc278fef563c77bada6efbee3815527d982e145
amfortin <amfortin@gosecure.ca>
2018-02-19 21:26:19 -0500

    No need for stubs list view works now

--- WifiSearcher/app/src/main/java/us/wifisearcher/WifiListActivity.java
+++ WifiSearcher/app/src/main/java/us/wifisearcher/WifiListActivity.java
@@ -8,9 +8,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import us.wifisearcher.persistence.database.WifiNetwork;
 
 public class WifiListActivity extends AppCompatActivity {
 
@@ -29,27 +26,6 @@ public class WifiListActivity extends AppCompatActivity {
         recyclerView.setAdapter(recyclerViewAdapter);
 
         viewModel = ViewModelProviders.of(this).get(WifiSearcherViewModel.class);
-
-        // TEMP %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        WifiNetwork w1 = new WifiNetwork();
-        w1.setName("Wifi test 1");
-        w1.setMacAddress("10:10:10:10");
-        w1.setSignalStrength(5);
-        WifiNetwork w2 = new WifiNetwork();
-        w2.setName("Wifi test 2");
-        w2.setMacAddress("20:20:20:20");
-        w2.setSignalStrength(3);
-        WifiNetwork w3 = new WifiNetwork();
-        w3.setName("Wifi test 3");
-        w3.setMacAddress("30:30:30:30");
-        w3.setSignalStrength(4);
-
-        List<WifiNetwork> defaultWifi = new ArrayList<>();
-        defaultWifi.add(w1);
-        defaultWifi.add(w2);
-        defaultWifi.add(w3);
-        recyclerViewAdapter.addItems(defaultWifi);
-        // ^^^^ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
         Resources res = getResources();
         recyclerViewAdapter.setWifi_mac_address(res.getString(R.string.wifi_mac_address));
