714bf96d59bbb4f552d707a8fac0a3286080e11d
amfortin <amfortin@gosecure.ca>
2018-02-28 21:06:41 -0500

    Parsing Wifi Security

--- WifiSearcher/app/src/main/java/us/wifisearcher/services/WifiLiveData.java
+++ WifiSearcher/app/src/main/java/us/wifisearcher/services/WifiLiveData.java
@@ -7,11 +7,10 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import us.wifisearcher.persistence.database.WifiNetwork;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import us.wifisearcher.persistence.database.WifiNetwork;
 
 import static android.net.wifi.WifiManager.calculateSignalLevel;
 
@@ -19,6 +18,14 @@ public class WifiLiveData extends LiveData<List<WifiNetwork>> {
     private WifiManager wifiManager;
     private Context context;
     private BroadcastReceiver broadcastReceiver;
+    // Constants used for different security types
+    private static final String WPA2 = "WPA2";
+    private static final String WPA = "WPA";
+    private static final String WEP = "WEP";
+    private static final String OPEN = "Open";
+    /* For EAP Enterprise fields */
+    private static final String WPA_EAP = "WPA-EAP";
+    private static final String IEEE8021X = "IEEE8021X";
 
     public WifiLiveData(Context context) {
         this.context = context;
@@ -32,23 +39,19 @@ public class WifiLiveData extends LiveData<List<WifiNetwork>> {
         };
     }
 
-    private void onScanResults(Context context, Intent intent) {
-        List<ScanResult> discoveredNetworks = wifiManager.getScanResults();
-        if (discoveredNetworks.isEmpty()) {
-            setValue(null);
-        } else {
-            List<WifiNetwork> networks = new ArrayList<>();
-            for (ScanResult scanResult : discoveredNetworks) {
-                WifiNetwork wifiNetwork = new WifiNetwork();
-                wifiNetwork.setName(scanResult.SSID);
-                wifiNetwork.setMacAddress(scanResult.BSSID);
-                wifiNetwork.setSignalStrength(calculateSignalLevel(scanResult.level, 5));
-                wifiNetwork.setEncryption(scanResult.capabilities);
-                networks.add(wifiNetwork);
+    /**
+     * @return The security of a given {@link ScanResult}.
+     */
+    public static String getScanResultSecurity(ScanResult scanResult) {
+        final String cap = scanResult.capabilities;
+        final String[] securityModes = {WEP, WPA, WPA2, WPA_EAP, IEEE8021X};
+        for (int i = securityModes.length - 1; i >= 0; i--) {
+            if (cap.contains(securityModes[i])) {
+                return securityModes[i];
             }
-
-            setValue(networks);
         }
+
+        return OPEN;
     }
 
     public void executeScan() {
@@ -66,5 +69,24 @@ public class WifiLiveData extends LiveData<List<WifiNetwork>> {
     protected void onInactive() {
         super.onInactive();
         this.context.unregisterReceiver(broadcastReceiver);
+    }
+
+    private void onScanResults(Context context, Intent intent) {
+        List<ScanResult> discoveredNetworks = wifiManager.getScanResults();
+        if (discoveredNetworks.isEmpty()) {
+            setValue(null);
+        } else {
+            List<WifiNetwork> networks = new ArrayList<>();
+            for (ScanResult scanResult : discoveredNetworks) {
+                WifiNetwork wifiNetwork = new WifiNetwork();
+                wifiNetwork.setName(scanResult.SSID);
+                wifiNetwork.setMacAddress(scanResult.BSSID);
+                wifiNetwork.setSignalStrength(calculateSignalLevel(scanResult.level, 5));
+                wifiNetwork.setEncryption(getScanResultSecurity(scanResult));
+                networks.add(wifiNetwork);
+            }
+
+            setValue(networks);
+        }
     }
 }
