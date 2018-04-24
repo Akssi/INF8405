e959c6033234e0a07c2e63f0b91a19e6ef5e1562
Alexis-Maurer Fortin <amfortin@gosecure.ca>
2018-04-24 17:13:23 -0400

    Proper Profile Activity

--- ZSX/app/src/main/java/zelemon/zsx/Game.java
+++ ZSX/app/src/main/java/zelemon/zsx/Game.java
@@ -11,39 +11,20 @@ import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.view.*;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.games.Games;
-import com.google.android.gms.games.GamesActivityResultCodes;
-import com.google.android.gms.games.GamesCallbackStatusCodes;
-import com.google.android.gms.games.GamesClient;
-import com.google.android.gms.games.GamesClientStatusCodes;
-import com.google.android.gms.games.InvitationsClient;
-import com.google.android.gms.games.PlayersClient;
-import com.google.android.gms.games.RealTimeMultiplayerClient;
+import com.google.android.gms.games.*;
 import com.google.android.gms.games.multiplayer.Invitation;
 import com.google.android.gms.games.multiplayer.InvitationCallback;
 import com.google.android.gms.games.multiplayer.Multiplayer;
 import com.google.android.gms.games.multiplayer.Participant;
-import com.google.android.gms.games.multiplayer.realtime.OnRealTimeMessageReceivedListener;
-import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
-import com.google.android.gms.games.multiplayer.realtime.Room;
-import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
-import com.google.android.gms.games.multiplayer.realtime.RoomStatusUpdateCallback;
-import com.google.android.gms.games.multiplayer.realtime.RoomUpdateCallback;
+import com.google.android.gms.games.multiplayer.realtime.*;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -52,16 +33,7 @@ import com.google.android.gms.tasks.Task;
 import java.math.BigDecimal;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 
 /**
@@ -635,6 +607,7 @@ public class Game extends AppCompatActivity implements
             case R.id.button_see_profile:
                 //switchToScreen(R.id.screen_wait);
                 Intent profileIntent = new Intent(this, ProfileActivity.class);
+                profileIntent.putExtra("signedInAccount", mSignedInAccount);
                 startActivity(profileIntent);
                 break;
         }
--- ZSX/app/src/main/java/zelemon/zsx/ProfileActivity.java
+++ ZSX/app/src/main/java/zelemon/zsx/ProfileActivity.java
@@ -1,15 +1,20 @@
 package zelemon.zsx;
 
+import android.Manifest;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.widget.Button;
 import android.widget.ImageView;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -24,14 +29,15 @@ import java.io.InputStream;
 
 public class ProfileActivity extends DaggerAppCompatActivity {
 
+    private static final int PERMISSION_REQUEST_LOCATION = 0;
     @Inject
     ViewModelProvider.Factory viewModelFactory;
     private TronViewModel tronViewModel;
     private ImageView mProfilePhoto;
     private String currentDisplayName;
     private Location currentLocation;
-    private final Observer<Location> locationObserver = this::updateCurrentLocationOnMap;
     private Profile currentProfile;
+    private final Observer<Location> locationObserver = this::updateCurrentLocation;
     private final Observer<Profile> currentProfileObserver = this::updateCurrentProfile;
     private LiveData<Profile> currentProfileLiveData;
 
@@ -40,30 +46,18 @@ public class ProfileActivity extends DaggerAppCompatActivity {
             currentProfile = profile;
             loadImageFromDatabase();
         } else {
-            this.currentProfile = new Profile(currentDisplayName, this.currentLocation, "");
-            this.tronViewModel.saveProfile(this.currentProfile);
-        }
-    }
-
-    private void updateCurrentLocationOnMap(Location location) {
-        this.currentLocation = location;
-    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-
-        if (account != null) {
-            currentDisplayName = account.getEmail();
-            if (currentDisplayName == null) {
-                currentDisplayName = "Name";
+            if (this.currentLocation != null) {
+                this.currentProfile = new Profile(currentDisplayName, this.currentLocation, "");
+                this.tronViewModel.saveProfile(this.currentProfile);
             }
         }
-        currentProfileLiveData = tronViewModel.getProfile(currentDisplayName);
-        currentProfileLiveData.observe(this, currentProfileObserver);
+    }
 
+    private void updateCurrentLocation(Location location) {
+        this.currentLocation = location;
+        if (this.currentProfile != null) {
+            this.currentProfile.setLocation(location);
+        }
     }
 
     @Override
@@ -72,7 +66,14 @@ public class ProfileActivity extends DaggerAppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         tronViewModel = ViewModelProviders.of(this, viewModelFactory).get(TronViewModel.class);
-        tronViewModel.getLocationLiveData().observe(this, locationObserver);
+
+        // Request Permission
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION);
+        } else {
+            inititalizeObservers();
+        }
 
         mProfilePhoto = findViewById(R.id.profile_photo);
         Button choosePhotoButton = findViewById(R.id.button_choose_photo);
@@ -81,14 +82,41 @@ public class ProfileActivity extends DaggerAppCompatActivity {
             photoPickerIntent.setType("image/*");
             startActivityForResult(photoPickerIntent, 0);
         });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSION_REQUEST_LOCATION:
+                if (grantResults.length > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    inititalizeObservers();
+                }
+        }
+    }
+
+
+    private void inititalizeObservers() {
+        tronViewModel.getLocationLiveData().observe(this, locationObserver);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account != null) {
+            currentDisplayName = account.getDisplayName();
+            if (currentDisplayName == null) {
+                currentDisplayName = "Name";
+            }
+        }
+        currentProfileLiveData = tronViewModel.getProfile(currentDisplayName);
+        currentProfileLiveData.observe(this, this.currentProfileObserver);
+
     }
 
 
     @Override
     protected void onActivityResult(int reqCode, int resultCode, Intent data) {
         super.onActivityResult(reqCode, resultCode, data);
-
-
         if (resultCode == RESULT_OK) {
             try {
                 final Uri imageUri = data.getData();
@@ -103,8 +131,13 @@ public class ProfileActivity extends DaggerAppCompatActivity {
     }
 
     private void saveToDatabase(Bitmap bitmapImage) {
-        this.currentProfile.setPicture(PictureTypeConverter.toString(bitmapImage));
-        this.tronViewModel.saveProfile(currentProfile);
+        if (this.currentProfile == null) {
+            this.currentProfile = new Profile(currentDisplayName, this.currentLocation, PictureTypeConverter.toString(bitmapImage));
+            this.tronViewModel.saveProfile(currentProfile);
+        } else {
+            this.currentProfile.setPicture(PictureTypeConverter.toString(bitmapImage));
+            this.tronViewModel.saveProfile(currentProfile);
+        }
     }
 
     private void loadImageFromDatabase() {
